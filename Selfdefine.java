e74e9363502ab7a9e565274894c291825c90378b
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-02 18:13:21 +0530

    Delete Selfdefine.java

--- Selfdefine.java
+++
@@ -1,8 +0,0 @@
-public class Selfdefine {
-    public static void main(String[] args) {
-        float first=11,second=21;
-        float div=first/second;
-        System.out.println(div);
-    }
-    
-}
