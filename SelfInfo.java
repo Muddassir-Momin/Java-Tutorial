69a8a77e88f0b10d436357b45e0488d03ea8fba0
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-02 18:12:06 +0530

    Add files via upload

---
+++ SelfInfo.java
@@ -0,0 +1,5 @@
+public class SelfInfo {
+    public static void main(String[] args) {
+        System.out.println("\n I am Muddassir Momin \n Second Year \n Computer Science and Engineering Student\n At Dr. JJ Magdum College Of Engineering");
+    }
+}
