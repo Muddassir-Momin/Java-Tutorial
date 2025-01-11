207e2a8f8bb331f6a4b4ff279935cbd67950e451
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-11 12:14:20 +0530

    Delete SelfDefineMul.java

--- SelfDefineMul.java
+++
@@ -1,7 +0,0 @@
-public class SelfDefineMul {
-    public static void main(String[] args) {
-        int a=20,b=2;                              //Self Define Multiplication
-        int mul=a*b;
-        System.out.println(mul);
-    }
-}
