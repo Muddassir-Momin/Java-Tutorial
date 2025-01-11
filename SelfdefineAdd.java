598c3145cd5207919a33ad4d0269603edcc3e0b2
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-11 12:15:15 +0530

    Delete SelfdefineAdd.java

--- SelfdefineAdd.java
+++
@@ -1,7 +0,0 @@
-public class SelfdefineAdd {
-    public static void main(String[] args) {
-        int a=10,b=10,sum;                               //Self Define Value
-        sum=a+b;
-        System.out.println(sum);
-    }
-}
