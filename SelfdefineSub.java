8cc12ed93824d39e4f308e34227c6cec2749dfdd
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-11 12:15:29 +0530

    Delete SelfdefineSub.java

--- SelfdefineSub.java
+++
@@ -1,7 +0,0 @@
-public class SelfdefineSub {
-    public static void main(String[] args) {
-        int a=10,b=20;                                              //Self Define Substraction
-        int sum=a-b;
-        System.out.println(sum);
-    }
-}
