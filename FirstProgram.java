1ef949d3e240973a00fe960c3553bbeeb5cdf9ba
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-11 13:49:39 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+// Hello World Program   which is Not User Define Program.
+
+public class FirstProgram                                //Class Name : FirstProgram (Name of File) 
+{                                                       // Start of Class
+    public static void main(String[] args)            // Main Function
+    {                                                // Start of Main Function
+        System.out.println("Hello World!");         // For Print the Statement we Written System.out.print (ln is use for Next line)
+    }                                              // End Of Main Function
+}                                                 // End Of Class
+
+
+// Compiler Start Execution From main Function
+//If there Class and main function Not Present then it will Not Execute
+
+
