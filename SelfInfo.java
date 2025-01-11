a21135702523dd2dfbff8c0500c6c6591e6492e8
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-11 12:15:04 +0530

    Delete SelfInfo.java

--- SelfInfo.java
+++
@@ -1,5 +0,0 @@
-public class SelfInfo {
-    public static void main(String[] args) {
-        System.out.println("\n I am Muddassir Momin \n Second Year \n Computer Science and Engineering Student\n At Dr. JJ Magdum College Of Engineering");
-    }
-}
