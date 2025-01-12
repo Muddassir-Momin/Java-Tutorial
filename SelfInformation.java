2733ff34eebe36e543d20e87584681f813a91290
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-12 15:49:05 +0530

    Add files via upload

---
+++ SelfInformation.java
@@ -0,0 +1,15 @@
+//Self Information Program which is Not taken from user
+
+public class SelfInformation  // public class (name of Our File)
+{                            // start of Class
+public static void main(String[] args)               // main function entry point if program
+{                                                    // start of main Funtion         Start Compilation
+System.out.println("My Self Muddassir\nSecond Year CSE Student at JJMCOE");   // For Print the Statement we Written System.out.print  
+System.out.println("In My First Year I Secured 7.32 CGPA\nCurrently I have 6 Months of Experince as an Intern"); //where ln is use for Next line
+}      //end of main Function                                End Complilation
+}      //end of Class
+
+
+// In this Program there Complier Start Executing Program From main Function
+//If there Class and main function Not Present then it will Not Execute
+
