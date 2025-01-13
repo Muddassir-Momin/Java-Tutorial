e886f79c70267d70faa311d0072f7e9032dcfc8c
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-13 20:38:19 +0530

    Add files via upload

---
+++ AdditionSelfDefine.java
@@ -0,0 +1,17 @@
+// Addition Of Two Numbers                         Which is not a User Define program
+
+public class AdditionSelfDefine                    // class AdditionSelfDefine  (name of File)
+{                                                  //Starting of Class
+    public static void main(String[] args)         //Main Function
+    {                                              //Starting of main Function                   start Compilation
+        int a=10;                                 // int is a Datatype Which is use for Storing the Integer Values
+        int b=20;                                 // where a,b,c is a Variables  where we assigned Number By using =
+        int c;                                    // where c will be store Result 
+        c=a+b;                                   // This is Expression   where We are Adding  a and b and Stores in C
+       System.out.println(c);                    // System.out.print is used for printing the Value After Addition Which Stored in C
+       System.out.println("Sum="+c);             //S.O.P is used for print where we printed Sum= and then we shown Value +c
+       System.out.println(a +"+"+ b +"="+c);       // here we printed a,b value and Result a+b=c    where output will be 10+20=30
+    }                                            // End of Main Function                          end Compilation
+}                                                // End of Class
+
+
