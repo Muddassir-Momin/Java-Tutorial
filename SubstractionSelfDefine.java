13d06c37caac21096e23403ca36afe41d99878b7
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-13 20:39:00 +0530

    Add files via upload

---
+++ SubstractionSelfDefine.java
@@ -0,0 +1,15 @@
+//Substraction Program Self Define 
+
+public class SubstractionSelfDefine      //class (File name)
+{                                             //Starting of Class
+    public static void main(String[] args)    //Main Function
+    {                                        //Starting of main Function                   start Compilation
+        int a=10,b=20,c;                   // int is a Datatype Which is use for Storing the Integer Values
+        c=a-b;                            // This is Expression   where We are Substract  a and b and Stores in C
+        System.out.println(c);
+        System.out.println("Substraction="+c); // System.out.print is used for printing the Value After Substraction Which Stored in C
+        System.out.println(a+"-"+b+"="+c);        // here we printed a,b value and Result a-b=c 
+    }         // End of Main Function                          end Compilation
+}                                                // End of Class
+
+
