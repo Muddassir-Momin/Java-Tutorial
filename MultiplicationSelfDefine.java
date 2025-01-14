8cf1dc6607b117a5c96d81d08039b69aa335ebeb
Muddassir-Momin <mominmuddassir01@gmail.com>
2025-01-14 20:07:59 +0530

    Add files via upload

---
+++ MultiplicationSelfDefine.java
@@ -0,0 +1,14 @@
+//Multiplication of Two Number    Self Define Program
+
+public class MultiplicationSelfDefine             //class (File name)
+{
+    public static void main(String[] args)          //Main Function
+    {                                              //Starting of main Function
+       int a=10,b=20;                                // int is a Datatype Which is use for Storing the Integer Values
+       int c=a*b;                                 // This is Expression   where We are multiplication  a and b and Stores in C
+       System.out.println(c);                    // System.out.print is used for printing the Value After Multiplication Which Stored in C
+       System.out.println("Multiplication="+c);
+       System.out.println(a+"*"+b+"="+c);            // here we printed a,b value and Result a*b=c 
+    }                                       // End of Main Function                          end Compilation
+}                                                // End of Class
+
