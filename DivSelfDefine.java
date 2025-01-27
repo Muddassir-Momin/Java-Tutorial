6b50b2dff124ecf6c733b9c729ea99fe2bbfa6b0
Muddassir Momin <mominmuddassir01@gmail.com>
2025-01-27 19:33:58 +0530

    Add files via upload

---
+++ DivSelfDefine.java
@@ -0,0 +1,12 @@
+//Self Define Division Program
+
+public class DivSelfDefine {
+    public static void main(String[] args) {
+        int a = 20, b = 20; // int is a Datatype Which is use for Storing the Integer Values
+        int c = a / b; // This is Expression where We are division a and b and Stores in C
+        System.out.println(c); // System.out.print is used for printing the Value After Division Which Stored in C
+        System.out.println("Division =" + c);
+        System.out.println(a + "/" + b + "=" + c); // here we printed a,b value and Result a*b=c
+    }
+}
+
