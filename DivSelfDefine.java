c6967deaaba6783979adf4c9a8f37802c64e0d38
Muddassir Momin <mominmuddassir01@gmail.com>
2025-01-27 19:35:10 +0530

    Update DivSelfDefine.java

--- DivSelfDefine.java
+++ DivSelfDefine.java
@@ -2,7 +2,7 @@
 
 public class DivSelfDefine {
     public static void main(String[] args) {
-        int a = 20, b = 20; // int is a Datatype Which is use for Storing the Integer Values
+        int a = 20, b = 20; // int is a Datatype Which is use for Storing the Integer Values a,b is Memory address
         int c = a / b; // This is Expression where We are division a and b and Stores in C
         System.out.println(c); // System.out.print is used for printing the Value After Division Which Stored in C
         System.out.println("Division =" + c);
